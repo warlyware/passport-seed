f36d9e4baf85d6d5e6ec492d9a2f17d6b89c92c0
warlyware <danward@gmail.com>
2015-08-21 19:10:38 -0400

    Fix google auth vars

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,9 @@
 module.exports = {
   'googleAuth': {
-    'clientID': '1098573959131-h0ot926rn50lvk4gh4iqc3kviv2jethl.apps.googleusercontent.com',
-    'clientSecret': 'AoeDlLw1nlHsgd-x1E2ph3XH',
+    // 'clientID': '1098573959131-h0ot926rn50lvk4gh4iqc3kviv2jethl.apps.googleusercontent.com',
+    // 'clientSecret': 'AoeDlLw1nlHsgd-x1E2ph3XH',
+    'clientID': process.env.GOOGLE_ID,
+    'clientSecret': process.env.GOOGLE_SECRET,
     'callbackURL': 'http://localhost:3000/auth/google/callback'
   }
 };
