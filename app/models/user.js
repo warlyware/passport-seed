482436cd6eb9dd2b33b6988f2462a90e9da23b71
warlyware <danward@gmail.com>
2015-08-22 12:15:36 -0400

    Add broken commit trying to implement github auth

--- app/models/user.js
+++ app/models/user.js
@@ -12,6 +12,12 @@ var userSchema = mongoose.Schema({
     token: String,
     email: String,
     name: String
+  },
+  github: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
   }
 
 });
--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,12 @@
 module.exports = {
   'googleAuth': {
-    // 'clientID': '1098573959131-h0ot926rn50lvk4gh4iqc3kviv2jethl.apps.googleusercontent.com',
-    // 'clientSecret': 'AoeDlLw1nlHsgd-x1E2ph3XH',
     'clientID': process.env.GOOGLE_ID,
     'clientSecret': process.env.GOOGLE_SECRET,
     'callbackURL': 'http://localhost:3000/auth/google/callback'
+  },
+  'githubAuth': {
+    'clientID': process.env.GITHUB_CLIENT_ID,
+    'clientSecret': process.env.GITHUB_CLIENT_SECRET,
+    'callbackURL': "http://localhost:3000/auth/github/callback"
   }
 };
